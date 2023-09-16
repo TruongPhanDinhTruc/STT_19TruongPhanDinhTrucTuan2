a36eca2dc1a9a1318bb0ade273cacaa4050f249d
TruongPhanDinhTruc <truongphandinhtruc@gmail.com>
2023-09-17 00:37:27 +0700

    tuan2

---
+++ test.js
@@ -0,0 +1,114 @@
+const urlBacon =
+    "https://jsonplaceholder.typicode.com/todos/1";
+
+// (function() {
+//     $.ajax({
+
+//         method: "GET",
+
+//         url: urlBacon,
+
+//         dataType: "json"
+
+//     })
+
+//     .done(function(data) {
+
+//         console.log(data);
+
+//     })
+
+//     .fail(function() {
+
+//         alert("no good");
+
+//     });
+
+// })()
+
+(function() {
+    $.ajax({
+
+        url: "https://jsonplaceholder.typicode.com/todos/1",
+
+        beforeSend: function(xhr) {
+
+            xhr.overrideMimeType("text/plain;charset=x-user-defined");
+
+        }
+
+    })
+
+    .done(function(data) {
+
+        if (console && console.log) {
+
+            console.log("Sample of data:",
+                data.slice(0, 100));
+
+        }
+
+    });
+})()
+
+//var url = "http://localhost:8080/api/v1/users";
+
+// var url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+// var xhr = new XMLHttpRequest()
+
+// xhr.open('GET', url, true)
+
+// xhr.onload = function() {
+
+//     var users = JSON.parse(xhr.responseText);
+
+//     if (xhr.readyState == 4 && xhr.status == "200") {
+
+//         console.table(users);
+
+//     } else {
+
+//         console.error(users);
+
+//     }
+
+// }
+
+// xhr.send(null);
+
+// // Post a user
+
+// var url = "http://localhost:8080/api/v1/users";
+
+var url = 'https://my-json-server.typicode.com/typicode/demo/posts';
+
+var data = {};
+
+// data.firstname = "John";
+
+// data.lastname = "Snow";
+
+var json = JSON.stringify(data);
+
+var xhr = new XMLHttpRequest();
+
+xhr.open("POST", url, true);
+
+xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+xhr.onload = function() {
+
+    var users = JSON.parse(xhr.responseText);
+
+    if (xhr.readyState == 4 && xhr.status == "201") {
+
+        console.table(users);
+
+    } else {
+
+        console.error(users);
+
+    }
+
+}
